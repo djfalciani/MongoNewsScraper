56b18c273396492e6a177f28712b0d565b0fe1cd
Danny Falciani <Daniel@danny-falcianis-macbook-pro.local>
2019-09-29 21:46:08 -0400

    Added Note Dialog

--- public/app.js
+++ public/app.js
@@ -10,11 +10,11 @@ $.getJSON("/articles", function(data) {
 
 function renderCard(id,title,teaser,link) {
   const articleDiv = $("#articleDiv");
-  const card = $('<div class="card">');
+  const card = $('<div class="card border-dark mb-3">');
   const cardHeader = $(`<div class="d-flex justify-content-between card-header">`)
   const articleTitle = $(`<div class="col-6"><h5><a href="${link}">${title}</a></h5></div>`);
   const saveButton = $(`<div class="col-6"><button class="btn btn-primary" type="button" id="saveArticle">Save Article</button></div>`);
-  const cardBody = $(`<div class="card-body"><p data-id="${id}">${teaser}</p></div>`);
+  const cardBody = $(`<div class="card-body"><p data-id="${id}">${teaser}</p><button type="button" class="btn btn-info" id="addArticle" data-id="${id}" data-toggle="modal" data-target="#exampleModal">Article Notes</button></div>`);
 
   // render card...
   // inner to outer...
@@ -29,24 +29,44 @@ function renderCard(id,title,teaser,link) {
 $("#scrapeBtn").on("click", function() {
   console.log("Scrape Button Clicked");
   $.ajax({
-    method: "POST",
+    method: "GET",
     url: "/scrapeNPR"})
     .then(function(data) {
       console.log(data);
     });
+
+    // Reload Page...
+    location.reload();
 });
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
+$("#deleteArticleBtn").on("click", function() {
+  console.log("Delete Button Clicked");
+  $.ajax({
+    method: "GET",
+    url: "/delete"})
+    .then(function(data) {
+      console.log(data);
+    });
+
+    // Reload Page...
+    location.reload();
+});
+
+// If user clicks the article button then display the modal...
+$(document).on("click", "#addArticle", function() {
+  // 1. Get the _id
+  var mongoId = $(this).attr("data-id");
+
+  // 2. Clear any information that was already in the modal...
   $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
+
+  // Populate the modal with Note Information...
+  $("#modalTitle").text(`Article Notes for #: ${mongoId}`);
 
   // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
-    url: "/articles/" + thisId
+    url: "/articles/" + mongoId
   })
     // With that done, add the note information to the page
     .then(function(data) {
@@ -54,11 +74,12 @@ $(document).on("click", "p", function() {
       // The title of the article
       $("#notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<input id='titleinput' name='title'>");
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // Add data-id and id attributes to already existing Modal Save btn...
+      $("#save-note").attr("data-id", data._id);
 
       // If there's a note in the article
       if (data.note) {
@@ -68,10 +89,11 @@ $(document).on("click", "p", function() {
         $("#bodyinput").val(data.note.body);
       }
     });
+
 });
 
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#save-note", function() {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
